2c284458823a3b5342edfe7053585d9dbc5307f4
vphone <veronique.phone@gmail.com>
2021-12-12 20:26:58 +0100

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <star :active="false"/>
+  </div>
+</template>
+
+<script>
+import Star from './components/Star.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Star
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/Star.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="star">
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Star',
+  props: {
+    active: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
